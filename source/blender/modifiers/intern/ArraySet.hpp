a2b169228889de6aed6dbba6fa7e48f765a8e1b4
Jacques Lucke <mail@jlucke.com>
2019-01-05 12:11:17 +0100

    use tabs

--- source/blender/modifiers/intern/ArraySet.hpp
+++ source/blender/modifiers/intern/ArraySet.hpp
@@ -5,57 +5,57 @@ template<typename T>
 class
 ArraySet {
 private:
-    using entries_t = std::vector<T>;
-    entries_t entries;
+	using entries_t = std::vector<T>;
+	entries_t entries;
 
 public:
-    using const_iterator = typename entries_t::const_iterator;
+	using const_iterator = typename entries_t::const_iterator;
 
-    ArraySet() {};
+	ArraySet() {};
 
-    ArraySet(std::vector<T> values)
-    {
-        for (T value : values) {
-            this->add(value);
-        }
-    }
+	ArraySet(std::vector<T> values)
+	{
+		for (T value : values) {
+			this->add(value);
+		}
+	}
 
 
-    ArraySet(std::initializer_list<T> values)
-    {
-        for (T value : values) {
-            this->add(value);
-        }
-    }
+	ArraySet(std::initializer_list<T> values)
+	{
+		for (T value : values) {
+			this->add(value);
+		}
+	}
 
-    void add(T value)
-    {
-        if (!this->contains(value)) {
-            this->entries.push_back(value);
-        }
-    }
+	void add(T value)
+	{
+		if (!this->contains(value)) {
+			this->entries.push_back(value);
+		}
+	}
 
-    bool contains(T value) const
-    {
-        for (T entry : this->entries) {
-            if (entry == value) return true;
-        }
-        return false;
-    }
+	bool contains(T value) const
+	{
+		for (T entry : this->entries) {
+			if (entry == value) return true;
+		}
+		return false;
+	}
 
-    T operator[](const int index) const
-    {
-        assert(index >= 0 && index < this->size());
-        return this->entries[index];
-    }
+	T operator[](const int index) const
+	{
+		assert(index >= 0 && index < this->size());
+		return this->entries[index];
+	}
 
-    uint size() const
-    {
-        return this->entries.size();
-    }
+	uint size() const
+	{
+		return this->entries.size();
+	}
 
-    const_iterator begin()
-    { return this->entries.begin(); }
-    const_iterator end()
-    { return this->entries.end(); }
+	const_iterator begin()
+	{ return this->entries.begin(); }
+	const_iterator end()
+	{ return this->entries.end(); }
 };
