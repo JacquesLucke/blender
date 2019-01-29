65cca0c766ba14e5cd6587b9f7e2b0a3717065f4
Jacques Lucke <mail@jlucke.com>
2019-01-29 20:38:48 +0100

    bring back timer

--- source/blender/modifiers/intern/MOD_rigiddeform_system.cc
+++ source/blender/modifiers/intern/MOD_rigiddeform_system.cc
@@ -26,6 +26,36 @@
 #include "MOD_rigiddeform_system.hpp"
 #include "BLI_math.h"
 
+#include <chrono>
+
+/* ************** Timer ***************** */
+
+class Timer {
+    const char *name;
+    std::chrono::high_resolution_clock::time_point start, end;
+    std::chrono::duration<float> duration;
+
+public:
+    Timer(const char *name = "");
+    ~Timer();
+};
+
+Timer::Timer(const char *name) {
+    this->name = name;
+    this->start = std::chrono::high_resolution_clock::now();
+}
+
+Timer::~Timer() {
+    end = std::chrono::high_resolution_clock::now();
+    duration = end - start;
+    double ms = duration.count() * 1000.0f;
+    std::cout << "Timer '" << name << "' took " << ms << " ms" << std::endl;
+}
+
+#define TIMEIT(name) Timer t(name);
+
+/* ************ Timer End *************** */
+
 namespace RigidDeform {
 
 	/* expects the anchor indices to be sorted */
