4688ed1e28c2125a73ee2eba43cb1b8791c8f82b
Jacques Lucke <mail@jlucke.com>
2019-03-10 14:27:22 +0100

    use ctrl+A to open the node search

---
+++ release/scripts/startup/function_nodes/keymap.py
@@ -0,0 +1,15 @@
+import bpy
+
+def register():
+    wm = bpy.context.window_manager
+
+    km = wm.keyconfigs.addon.keymaps.new(
+        name="Node Editor",
+        space_type='NODE_EDITOR')
+
+    km.keymap_items.new(
+        "fn.node_search",
+        type='A',
+        value='PRESS',
+        ctrl=True)
+
