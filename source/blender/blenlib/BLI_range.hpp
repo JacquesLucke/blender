844745da773b3c9924a18ab5dad8c9f453bf6598
Jacques Lucke <mail@jlucke.com>
2019-04-19 13:56:49 +0200

    Simple range iterator with tests

---
+++ source/blender/blenlib/BLI_range.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "BLI_utildefines.h"
+
+namespace BLI {
+
+template<typename T> class Range {
+ private:
+  T m_start = 0;
+  T m_one_after_last = 0;
+
+ public:
+  Range() = default;
+
+  Range(T start, T one_after_last) : m_start(start), m_one_after_last(one_after_last)
+  {
+    BLI_assert(start <= one_after_last);
+  }
+
+  class RangeIterator {
+   private:
+    const Range &m_range;
+    T m_current;
+
+   public:
+    RangeIterator(const Range &range, T current) : m_range(range), m_current(current)
+    {
+    }
+
+    RangeIterator &operator++()
+    {
+      m_current++;
+      return *this;
+    }
+
+    bool operator!=(const RangeIterator &iterator) const
+    {
+      return m_current != iterator.m_current;
+    }
+
+    T operator*() const
+    {
+      return m_current;
+    }
+  };
+
+  RangeIterator begin() const
+  {
+    return RangeIterator(*this, m_start);
+  }
+
+  RangeIterator end() const
+  {
+    return RangeIterator(*this, m_one_after_last);
+  }
+
+  uint size() const
+  {
+    return m_one_after_last - m_start;
+  }
+};
+}  // namespace BLI
---
+++ tests/gtests/blenlib/BLI_range_test.cc
@@ -0,0 +1,49 @@
+#include "testing/testing.h"
+#include "BLI_range.hpp"
+#include "BLI_small_vector.hpp"
+
+using IntRange = BLI::Range<int>;
+using IntVector = BLI::SmallVector<int>;
+
+TEST(range, DefaultConstructor)
+{
+  IntRange range;
+  EXPECT_EQ(range.size(), 0);
+
+  IntVector vector;
+  for (int value : range) {
+    vector.append(value);
+  }
+  EXPECT_EQ(vector.size(), 0);
+}
+
+TEST(range, SingleElementRange)
+{
+  IntRange range(4, 5);
+  EXPECT_EQ(range.size(), 1);
+  EXPECT_EQ(*range.begin(), 4);
+
+  IntVector vector;
+  for (int value : range) {
+    vector.append(value);
+  }
+
+  EXPECT_EQ(vector.size(), 1);
+  EXPECT_EQ(vector[0], 4);
+}
+
+TEST(range, MultipleElementRange)
+{
+  IntRange range(6, 10);
+  EXPECT_EQ(range.size(), 4);
+
+  IntVector vector;
+  for (int value : range) {
+    vector.append(value);
+  }
+
+  EXPECT_EQ(vector.size(), 4);
+  for (uint i = 0; i < 4; i++) {
+    EXPECT_EQ(vector[i], i + 6);
+  }
+}
