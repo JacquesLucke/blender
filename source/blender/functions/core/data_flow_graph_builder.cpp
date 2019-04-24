700eacb30d7b9baf2ccaa9c5df1d3918bf542982
Jacques Lucke <mail@jlucke.com>
2019-04-24 17:19:28 +0200

    initial data flow graph builder

---
+++ source/blender/functions/core/data_flow_graph_builder.cpp
@@ -0,0 +1,73 @@
+#include "data_flow_graph_builder.hpp"
+
+namespace FN {
+
+SharedType &DFGB_Socket::type() const
+{
+  if (m_is_output) {
+    return this->node()->signature().outputs()[m_index].type();
+  }
+  else {
+    return this->node()->signature().inputs()[m_index].type();
+  }
+}
+
+std::string DFGB_Socket::name() const
+{
+  if (m_is_output) {
+    return this->node()->signature().outputs()[m_index].name();
+  }
+  else {
+    return this->node()->signature().inputs()[m_index].name();
+  }
+}
+
+DataFlowGraphBuilder::DataFlowGraphBuilder() : m_node_pool(sizeof(DFGB_Node))
+{
+  m_source_info_pool = std::unique_ptr<MemMultiPool>(new MemMultiPool());
+}
+
+DFGB_Node *DataFlowGraphBuilder::insert_function(SharedFunction &fn, SourceInfo *source)
+{
+  void *ptr = m_node_pool.allocate();
+  DFGB_Node *node = new (ptr) DFGB_Node(*this, fn, source);
+  return node;
+}
+
+void DataFlowGraphBuilder::insert_link(DFGB_Socket a, DFGB_Socket b)
+{
+  BLI_assert(a.node() != b.node());
+  BLI_assert(a.type() == b.type());
+  BLI_assert(a.is_input() != b.is_input());
+  BLI_assert(&a.builder() == this && &b.builder() == this);
+
+  if (a.is_input()) {
+    BLI_assert(!m_input_origins.contains(a));
+    m_input_origins.add_new(a, b);
+    m_output_targets.add(b, a);
+  }
+  else {
+    BLI_assert(!m_input_origins.contains(b));
+    m_input_origins.add_new(b, a);
+    m_output_targets.add(a, b);
+  }
+}
+
+SmallVector<DFGB_Node *> DataFlowGraphBuilder::nodes()
+{
+  SmallVector<DFGB_Node *> nodes;
+  for (DFGB_Node *node : m_nodes) {
+    nodes.append(node);
+  }
+  return nodes;
+}
+
+SmallVector<DFGB_Link> DataFlowGraphBuilder::links()
+{
+  SmallVector<DFGB_Link> links;
+  for (auto item : m_input_origins.items()) {
+    links.append(DFGB_Link(item.value, item.key));
+  }
+}
+
+}  // namespace FN
---
+++ source/blender/functions/core/data_flow_graph_builder.hpp
@@ -0,0 +1,272 @@
+#pragma once
+
+#include "compact_data_flow_graph.hpp"
+#include "BLI_optional.hpp"
+
+namespace FN {
+
+class DFGB_Socket;
+class DFGB_Node;
+class DataFlowGraphBuilder;
+
+class DFGB_Socket {
+ public:
+  DFGB_Socket(DFGB_Node *node, bool is_output, uint index)
+      : m_node(node), m_is_output(is_output), m_index(index)
+  {
+  }
+
+  inline DFGB_Node *node() const;
+  inline bool is_input() const;
+  inline bool is_output() const;
+  inline uint index() const;
+  inline DataFlowGraphBuilder &builder();
+
+  SharedType &type() const;
+  std::string name() const;
+
+  inline Optional<DFGB_Socket> origin();
+  inline ArrayRef<DFGB_Socket> targets();
+  inline bool is_linked();
+
+  friend bool operator==(const DFGB_Socket &a, const DFGB_Socket &b);
+  friend std::ostream &operator<<(std::ostream &stream, DFGB_Socket socket);
+
+ private:
+  DFGB_Node *m_node;
+  bool m_is_output;
+  uint m_index;
+};
+
+class DFGB_Node {
+ public:
+  DFGB_Node(DataFlowGraphBuilder &builder, SharedFunction fn, SourceInfo *source)
+      : m_builder(builder), m_function(fn), m_source(source)
+  {
+  }
+
+  DataFlowGraphBuilder &builder() const;
+  Signature &signature();
+  SharedFunction &function();
+
+  uint input_amount() const;
+  uint output_amount() const;
+
+  SourceInfo *source() const;
+
+  class SocketIt {
+   private:
+    DFGB_Node *m_node;
+    bool m_is_output;
+    uint m_index;
+
+   public:
+    SocketIt(DFGB_Node *node, bool is_output, uint index)
+        : m_node(node), m_is_output(is_output), m_index(index)
+    {
+    }
+
+    SocketIt begin() const
+    {
+      return SocketIt(m_node, m_is_output, 0);
+    }
+    SocketIt end() const
+    {
+      Signature &sig = m_node->signature();
+      uint size = m_is_output ? sig.outputs().size() : sig.inputs().size();
+      return SocketIt(m_node, m_is_output, size);
+    }
+
+    SocketIt &operator++()
+    {
+      m_index++;
+      return *this;
+    }
+
+    bool operator!=(const SocketIt &other) const
+    {
+      BLI_assert(m_is_output == other.m_is_output);
+      BLI_assert(m_node == other.m_node);
+      return m_index != other.m_index;
+    }
+
+    DFGB_Socket operator*() const
+    {
+      return DFGB_Socket(m_node, m_is_output, m_index);
+    }
+  };
+
+ private:
+  DataFlowGraphBuilder &m_builder;
+  SharedFunction m_function;
+  SourceInfo *m_source;
+};
+
+class DFGB_Link {
+ public:
+  DFGB_Link(DFGB_Socket from, DFGB_Socket to) : m_from(from), m_to(to)
+  {
+    BLI_assert(from.is_output());
+    BLI_assert(to.is_input());
+  }
+
+  DFGB_Socket from()
+  {
+    return m_from;
+  }
+
+  DFGB_Socket to()
+  {
+    return m_to;
+  }
+
+  friend bool operator==(const DFGB_Link &a, const DFGB_Link &b)
+  {
+    return a.m_from == b.m_from && a.m_to == b.m_to;
+  }
+
+ private:
+  DFGB_Socket m_from, m_to;
+};
+
+class DataFlowGraphBuilder {
+ public:
+  DataFlowGraphBuilder();
+  DataFlowGraphBuilder(DataFlowGraphBuilder &other) = delete;
+
+  SharedCompactDataFlowGraph build();
+
+  DFGB_Node *insert_function(SharedFunction &fn, SourceInfo *source = nullptr);
+  void insert_link(DFGB_Socket a, DFGB_Socket b);
+
+  SmallVector<DFGB_Link> links();
+  SmallVector<DFGB_Node *> nodes();
+
+  template<typename T, typename... Args> T *new_source_info(Args &&... args)
+  {
+    // TODO
+  }
+
+ private:
+  SmallSet<DFGB_Node *> m_nodes;
+  SmallMap<DFGB_Socket, DFGB_Socket> m_input_origins;
+  MultiMap<DFGB_Socket, DFGB_Socket> m_output_targets;
+  MemPool m_node_pool;
+  std::unique_ptr<MemMultiPool> m_source_info_pool;
+
+  friend DFGB_Socket;
+};
+
+/* Inline methods of Socket
+ ********************************************/
+
+inline DFGB_Node *DFGB_Socket::node() const
+{
+  return m_node;
+}
+
+inline DataFlowGraphBuilder &DFGB_Socket::builder()
+{
+  return m_node->builder();
+}
+
+inline bool DFGB_Socket::is_input() const
+{
+  return !m_is_output;
+}
+
+inline bool DFGB_Socket::is_output() const
+{
+  return m_is_output;
+}
+
+inline uint DFGB_Socket::index() const
+{
+  return m_index;
+}
+
+inline Optional<DFGB_Socket> DFGB_Socket::origin()
+{
+  BLI_assert(this->is_input());
+  DFGB_Socket *socket = this->builder().m_input_origins.lookup_ptr(*this);
+  return Optional<DFGB_Socket>::FromPointer(socket);
+}
+inline ArrayRef<DFGB_Socket> DFGB_Socket::targets()
+{
+  BLI_assert(this->is_output());
+  auto targets = this->builder().m_output_targets.lookup_default(*this);
+  return targets;
+}
+
+inline bool DFGB_Socket::is_linked()
+{
+  if (this->is_input()) {
+    return this->builder().m_input_origins.contains(*this);
+  }
+  else {
+    return this->builder().m_output_targets.has_at_least_one_value(*this);
+  }
+}
+
+inline bool operator==(const DFGB_Socket &a, const DFGB_Socket &b)
+{
+  return (a.m_node == b.m_node && a.m_is_output == b.m_is_output && a.m_index == b.m_index);
+}
+
+inline std::ostream &operator<<(std::ostream &stream, DFGB_Socket socket)
+{
+  stream << "<" << socket.node()->function()->name();
+  stream << ", " << ((socket.is_input()) ? "Input" : "Output");
+  stream << ":" << socket.index() << ">";
+  return stream;
+}
+
+/* Inline methods of Node
+ ********************************************/
+
+inline DataFlowGraphBuilder &DFGB_Node::builder() const
+{
+  return m_builder;
+}
+
+inline Signature &DFGB_Node::signature()
+{
+  return m_function->signature();
+}
+
+inline SharedFunction &DFGB_Node::function()
+{
+  return m_function;
+}
+
+inline uint DFGB_Node::input_amount() const
+{
+  return m_function->signature().inputs().size();
+}
+
+inline uint DFGB_Node::output_amount() const
+{
+  return m_function->signature().outputs().size();
+}
+
+inline SourceInfo *DFGB_Node::source() const
+{
+  return m_source;
+}
+
+}  // namespace FN
+
+namespace std {
+template<> struct hash<FN::DFGB_Socket> {
+  typedef FN::DFGB_Socket argument_type;
+  typedef size_t result_type;
+
+  result_type operator()(argument_type const &v) const noexcept
+  {
+    size_t h1 = std::hash<FN::DFGB_Node *>{}(v.node());
+    size_t h2 = std::hash<bool>{}(v.is_input());
+    size_t h3 = std::hash<uint>{}(v.index());
+    return h1 ^ h2 ^ h3;
+  }
+};
+}  // namespace std
