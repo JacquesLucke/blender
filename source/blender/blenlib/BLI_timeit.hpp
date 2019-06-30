8e6bdabb004f6225281f617a313ec5c9b9aa2bdd
Jacques Lucke <mail@jlucke.com>
2019-06-30 11:27:18 +0200

    always display nanoseconds for now

--- source/blender/blenlib/BLI_timeit.hpp
+++ source/blender/blenlib/BLI_timeit.hpp
@@ -17,7 +17,7 @@ using Nanoseconds = std::chrono::nanoseconds;
 
 inline void print_duration(Nanoseconds duration)
 {
-  if (duration.count() < 10000) {
+  if (duration.count() < 10000000) {
     std::cout << duration.count() << " ns";
   }
   else {
