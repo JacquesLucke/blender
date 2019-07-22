d6b7aecbbc99f8f53a8ae479ba4af8055df364c3
Jacques Lucke <mail@jlucke.com>
2019-07-22 17:58:31 +0200

    reorder lines

--- source/blender/simulations/bparticles/inserters.hpp
+++ source/blender/simulations/bparticles/inserters.hpp
@@ -36,21 +36,18 @@ struct BuildContext {
 using ForceFromNodeCallback =
     std::function<std::unique_ptr<Force>(BuildContext &ctx, VirtualNode *vnode)>;
 
-StringMap<ForceFromNodeCallback> &get_force_builders();
-
 using EventFromNodeCallback =
     std::function<std::unique_ptr<Event>(BuildContext &ctx, VirtualNode *vnode)>;
-
-StringMap<EventFromNodeCallback> &get_event_builders();
 
 using EmitterFromNodeCallback = std::function<std::unique_ptr<Emitter>(
     BuildContext &ctx, VirtualNode *vnode, StringRef particle_type_name)>;
 
-StringMap<EmitterFromNodeCallback> &get_emitter_builders();
-
 using OffsetHandlerFromNodeCallback =
     std::function<std::unique_ptr<OffsetHandler>(BuildContext &ctx, VirtualNode *vnode)>;
 
+StringMap<ForceFromNodeCallback> &get_force_builders();
+StringMap<EventFromNodeCallback> &get_event_builders();
+StringMap<EmitterFromNodeCallback> &get_emitter_builders();
 StringMap<OffsetHandlerFromNodeCallback> &get_offset_handler_builders();
 
 }  // namespace BParticles
