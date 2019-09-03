61ec4a3d89fd5702f017c1e7733dbcdec2ffebba
Jacques Lucke <mail@jlucke.com>
2019-09-03 11:20:04 +0200

    move change color/velocity node to same file

--- release/scripts/startup/nodes/bparticle_nodes/change_attribute.py
+++ release/scripts/startup/nodes/bparticle_nodes/change_attribute.py
@@ -3,6 +3,16 @@ from bpy.props import *
 from .. base import BParticlesNode
 from .. node_builder import NodeBuilder
 
+
+class ChangeParticleColorNode(bpy.types.Node, BParticlesNode):
+    bl_idname = "bp_ChangeParticleColorNode"
+    bl_label = "Change Particle Color"
+
+    def declaration(self, builder: NodeBuilder):
+        builder.fixed_input("color", "Color", "Color")
+        builder.execute_output("execute", "Execute")
+
+
 class ChangeParticleVelocityNode(bpy.types.Node, BParticlesNode):
     bl_idname = "bp_ChangeParticleVelocityNode"
     bl_label = "Change Particle Velocity"
