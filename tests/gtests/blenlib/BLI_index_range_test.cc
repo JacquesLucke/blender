ded8e6f0afe8968440f2fa1e5e3a7cca763a0063
Jacques Lucke <mail@jlucke.com>
2019-09-05 12:17:47 +0200

    rename test files

--- tests/gtests/blenlib/BLI_index_range_test.cc
+++ tests/gtests/blenlib/BLI_index_range_test.cc
@@ -131,32 +131,3 @@ TEST(index_range, AsArrayRef)
   EXPECT_EQ(ref[2], 6);
   EXPECT_EQ(ref[3], 7);
 }
-
-TEST(chunked_range, ChunksExact)
-{
-  IndexRange range = IndexRange(10, 40);
-  ChunkedIndexRange chunked_range(range, 10);
-  EXPECT_EQ(chunked_range.chunks(), 4);
-
-  EXPECT_EQ(chunked_range.chunk_range(0), IndexRange(10, 10));
-  EXPECT_EQ(chunked_range.chunk_range(1), IndexRange(20, 10));
-  EXPECT_EQ(chunked_range.chunk_range(2), IndexRange(30, 10));
-  EXPECT_EQ(chunked_range.chunk_range(3), IndexRange(40, 10));
-}
-
-TEST(chunked_range, ChunksMore)
-{
-  IndexRange range = IndexRange(25, 15);
-  ChunkedIndexRange chunked_range(range, 10);
-  EXPECT_EQ(chunked_range.chunks(), 2);
-
-  EXPECT_EQ(chunked_range.chunk_range(0), IndexRange(25, 10));
-  EXPECT_EQ(chunked_range.chunk_range(1), IndexRange(35, 5));
-}
-
-TEST(chunked_range, ChunksZero)
-{
-  IndexRange range = IndexRange(20, 0);
-  ChunkedIndexRange chunked_range(range, 10);
-  EXPECT_EQ(chunked_range.chunks(), 0);
-}
