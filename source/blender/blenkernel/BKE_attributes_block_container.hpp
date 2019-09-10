1b3ee092dbf5b2b5d0d5dd5129f81de080c764f7
Jacques Lucke <mail@jlucke.com>
2019-09-10 12:39:18 +0200

    move attributes block container stuff to same file again

--- source/blender/blenkernel/BKE_attributes_block_container.hpp
+++ source/blender/blenkernel/BKE_attributes_block_container.hpp
@@ -3,9 +3,81 @@
 #include <mutex>
 #include <atomic>
 
-#include "BKE_attributes_block.hpp"
+#include "BLI_utility_mixins.hpp"
+
+#include "BKE_attributes_ref.hpp"
 
 namespace BKE {
+
+class AttributesBlockContainer;
+
+class AttributesBlock : BLI::NonCopyable, BLI::NonMovable {
+ private:
+  const AttributesInfo *m_attributes_info;
+  Vector<void *> m_buffers;
+  uint m_size;
+  uint m_capacity;
+  AttributesBlockContainer *m_owner;
+
+ public:
+  AttributesBlock(const AttributesInfo *attributes_info,
+                  uint capacity,
+                  AttributesBlockContainer &owner);
+  ~AttributesBlock();
+
+  void update_buffers(const AttributesInfo *new_info, const AttributesInfoDiff &info_diff);
+
+  uint size() const
+  {
+    return m_size;
+  }
+
+  uint capacity() const
+  {
+    return m_capacity;
+  }
+
+  uint remaining_capacity() const
+  {
+    return m_capacity - m_size;
+  }
+
+  IndexRange active_range() const
+  {
+    return IndexRange(m_size);
+  }
+
+  void set_size(uint new_size)
+  {
+    BLI_assert(new_size <= m_capacity);
+    m_size = new_size;
+  }
+
+  AttributesBlockContainer &owner()
+  {
+    return *m_owner;
+  }
+
+  void move(uint old_index, uint new_index);
+
+  static void MoveUntilFull(AttributesBlock &from, AttributesBlock &to);
+  static void Compress(MutableArrayRef<AttributesBlock *> blocks);
+
+  AttributesRef as_ref()
+  {
+    return AttributesRef(*this);
+  }
+
+  AttributesRef as_ref__all()
+  {
+    return AttributesRef(*m_attributes_info, m_buffers, m_capacity);
+  }
+
+  operator AttributesRef()
+  {
+    return AttributesRef(*m_attributes_info, m_buffers, m_size);
+  }
+};
 
 class AttributesBlockContainer : BLI::NonCopyable, BLI::NonMovable {
  private:
