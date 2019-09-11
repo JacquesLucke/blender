3f28239efef2d58018c23b7ec08d0cb8b4e7f5fd
Jacques Lucke <mail@jlucke.com>
2019-09-11 10:55:37 +0200

    move events ui to same file

--- release/scripts/startup/nodes/bparticle_nodes/events.py
+++ release/scripts/startup/nodes/bparticle_nodes/events.py
@@ -3,6 +3,7 @@ from bpy.props import *
 from .. base import BParticlesNode
 from .. node_builder import NodeBuilder
 
+
 class AgeReachedEventNode(bpy.types.Node, BParticlesNode):
     bl_idname = "bp_AgeReachedEventNode"
     bl_label = "Age Reached Event"
@@ -12,6 +13,19 @@ class AgeReachedEventNode(bpy.types.Node, BParticlesNode):
     def declaration(self, builder: NodeBuilder):
         builder.fixed_input("age", "Age", "Float", default=3)
         builder.fixed_input("variation", "Variation", "Float")
+        builder.execute_input("execute_on_event", "Execute on Event", "execute_on_event__prop")
+
+        builder.particle_effector_output("event", "Event")
+
+
+class MeshCollisionEventNode(bpy.types.Node, BParticlesNode):
+    bl_idname = "bp_MeshCollisionEventNode"
+    bl_label = "Mesh Collision Event"
+
+    execute_on_event__prop: NodeBuilder.ExecuteInputProperty()
+
+    def declaration(self, builder: NodeBuilder):
+        builder.fixed_input("object", "Object", "Object")
         builder.execute_input("execute_on_event", "Execute on Event", "execute_on_event__prop")
 
         builder.particle_effector_output("event", "Event")
