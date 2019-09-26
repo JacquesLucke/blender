353d6112d25ac6a059ee870c659c282867214011
Jacques Lucke <mail@jlucke.com>
2019-09-26 12:06:20 +0200

    use header guards

--- source/blender/blenkernel/BKE_virtual_node_tree_cxx.h
+++ source/blender/blenkernel/BKE_virtual_node_tree_cxx.h
@@ -1,4 +1,25 @@
-#pragma once
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __BKE_VIRTUAL_NODE_TREE_CXX_H__
+#define __BKE_VIRTUAL_NODE_TREE_CXX_H__
+
+/** \file
+ * \ingroup bke
+ */
 
 #include "DNA_node_types.h"
 
@@ -269,3 +290,5 @@ inline VirtualSocket *VirtualNode::output(uint index, StringRef expected_name)
 }
 
 }  // namespace BKE
+
+#endif /* __BKE_VIRTUAL_NODE_TREE_CXX_H__ */
