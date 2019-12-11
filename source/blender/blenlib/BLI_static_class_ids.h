6d3cd34bd518220f3fa3bc9bc687836a490fcb8b
Jacques Lucke <mail@jlucke.com>
2019-12-11 17:13:16 +0100

    fix compile error

--- source/blender/blenlib/BLI_static_class_ids.h
+++ source/blender/blenlib/BLI_static_class_ids.h
@@ -12,11 +12,13 @@ template<typename T> class_id_t get_class_id();
 }  // namespace BLI
 
 #define BLI_CREATE_CLASS_ID_UTIL1(class_name, id) \
+  namespace BLI { \
   static char class_id_char##id = 0; \
-  static BLI::class_id_t class_id##id = (BLI::class_id_t)&class_id_char##id; \
-  template<> BLI::class_id_t BLI::get_class_id<class_name>() \
+  static class_id_t class_id##id = (class_id_t)&class_id_char##id; \
+  template<> class_id_t get_class_id<class_name>() \
   { \
     return class_id##id; \
+  } \
   }
 
 #define BLI_CREATE_CLASS_ID_UTIL2(class_name, id) BLI_CREATE_CLASS_ID_UTIL1(class_name, id)
