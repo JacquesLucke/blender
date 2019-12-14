b52c4ddfb94d9c19d6f40a8627fa5779d88b6409
Jacques Lucke <mail@jlucke.com>
2019-12-14 17:44:56 +0100

    more low level generic CPPType functions

--- source/blender/functions/FN_cpp_type.h
+++ source/blender/functions/FN_cpp_type.h
@@ -7,6 +7,7 @@
 
 namespace FN {
 
+using BLI::ArrayRef;
 using BLI::StringRef;
 using BLI::StringRefNull;
 
@@ -14,18 +15,33 @@ class CPPType {
  public:
   using ConstructDefaultF = void (*)(void *ptr);
   using ConstructDefaultNF = void (*)(void *ptr, uint n);
+  using ConstructDefaultIndicesF = void (*)(void *ptr, ArrayRef<uint> indices);
+
   using DestructF = void (*)(void *ptr);
   using DestructNF = void (*)(void *ptr, uint n);
+  using DestructIndicesF = void (*)(void *ptr, ArrayRef<uint> indices);
+
   using CopyToInitializedF = void (*)(const void *src, void *dst);
   using CopyToInitializedNF = void (*)(const void *src, void *dst, uint n);
+  using CopyToInitializedIndicesF = void (*)(const void *src, void *dst, ArrayRef<uint> indices);
+
   using CopyToUninitializedF = void (*)(const void *src, void *dst);
   using CopyToUninitializedNF = void (*)(const void *src, void *dst, uint n);
+  using CopyToUninitializedIndicesF = void (*)(const void *src, void *dst, ArrayRef<uint> indices);
+
   using RelocateToInitializedF = void (*)(void *src, void *dst);
   using RelocateToInitializedNF = void (*)(void *src, void *dst, uint n);
+  using RelocateToInitializedIndicesF = void (*)(void *src, void *dst, ArrayRef<uint> indices);
+
   using RelocateToUninitializedF = void (*)(void *src, void *dst);
   using RelocateToUninitializedNF = void (*)(void *src, void *dst, uint n);
+  using RelocateToUninitializedIndicesF = void (*)(void *src, void *dst, ArrayRef<uint> indices);
+
   using FillInitializedF = void (*)(const void *value, void *dst, uint n);
+  using FillInitializedIndicesF = void (*)(const void *value, void *dst, ArrayRef<uint> indices);
+
   using FillUninitializedF = void (*)(const void *value, void *dst, uint n);
+  using FillUninitializedIndicesF = void (*)(const void *value, void *dst, ArrayRef<uint> indices);
 
   CPPType(std::string name,
           uint size,
@@ -33,36 +49,52 @@ class CPPType {
           bool trivially_destructible,
           ConstructDefaultF construct_default,
           ConstructDefaultNF construct_default_n,
+          ConstructDefaultIndicesF construct_default_indices,
           DestructF destruct,
           DestructNF destruct_n,
+          DestructIndicesF destruct_indices,
           CopyToInitializedF copy_to_initialized,
           CopyToInitializedNF copy_to_initialized_n,
+          CopyToInitializedIndicesF copy_to_initialized_indices,
           CopyToUninitializedF copy_to_uninitialized,
           CopyToUninitializedNF copy_to_uninitialized_n,
+          CopyToUninitializedIndicesF copy_to_uninitialized_indices,
           RelocateToInitializedF relocate_to_initialized,
           RelocateToInitializedNF relocate_to_initialized_n,
+          RelocateToInitializedIndicesF relocate_to_initialized_indices,
           RelocateToUninitializedF relocate_to_uninitialized,
           RelocateToUninitializedNF relocate_to_uninitialized_n,
+          RelocateToUninitializedIndicesF relocate_to_uninitialized_indices,
           FillInitializedF fill_initialized,
+          FillInitializedIndicesF fill_initialized_indices,
           FillUninitializedF fill_uninitialized,
+          FillUninitializedIndicesF fill_uninitialized_indices,
           const CPPType *generalization)
       : m_size(size),
         m_alignment(alignment),
         m_trivially_destructible(trivially_destructible),
         m_construct_default(construct_default),
         m_construct_default_n(construct_default_n),
+        m_construct_default_indices(construct_default_indices),
         m_destruct(destruct),
         m_destruct_n(destruct_n),
+        m_destruct_indices(destruct_indices),
         m_copy_to_initialized(copy_to_initialized),
         m_copy_to_initialized_n(copy_to_initialized_n),
+        m_copy_to_initialized_indices(copy_to_initialized_indices),
         m_copy_to_uninitialized(copy_to_uninitialized),
         m_copy_to_uninitialized_n(copy_to_uninitialized_n),
+        m_copy_to_uninitialized_indices(copy_to_uninitialized_indices),
         m_relocate_to_initialized(relocate_to_initialized),
         m_relocate_to_initialized_n(relocate_to_initialized_n),
+        m_relocate_to_initialized_indices(relocate_to_initialized_indices),
         m_relocate_to_uninitialized(relocate_to_uninitialized),
         m_relocate_to_uninitialized_n(relocate_to_uninitialized_n),
+        m_relocate_to_uninitialized_indices(relocate_to_uninitialized_indices),
         m_fill_initialized(fill_initialized),
+        m_fill_initialized_indices(fill_initialized_indices),
         m_fill_uninitialized(fill_uninitialized),
+        m_fill_uninitialized_indices(fill_uninitialized_indices),
         m_generalization(generalization),
         m_name(name)
   {
@@ -119,6 +151,13 @@ class CPPType {
     m_construct_default_n(ptr, n);
   }
 
+  void construct_default_indices(void *ptr, ArrayRef<uint> indices) const
+  {
+    BLI_assert(this->pointer_has_valid_alignment(ptr));
+
+    m_construct_default_indices(ptr, indices);
+  }
+
   void destruct(void *ptr) const
   {
     BLI_assert(this->pointer_has_valid_alignment(ptr));
@@ -131,6 +170,13 @@ class CPPType {
     BLI_assert(this->pointer_has_valid_alignment(ptr));
 
     m_destruct_n(ptr, n);
+  }
+
+  void destruct_indices(void *ptr, ArrayRef<uint> indices) const
+  {
+    BLI_assert(this->pointer_has_valid_alignment(ptr));
+
+    m_destruct_indices(ptr, indices);
   }
 
   void copy_to_initialized(const void *src, void *dst) const
@@ -149,6 +195,14 @@ class CPPType {
     m_copy_to_initialized_n(src, dst, n);
   }
 
+  void copy_to_initialized_indices(const void *src, void *dst, ArrayRef<uint> indices) const
+  {
+    BLI_assert(this->pointer_has_valid_alignment(src));
+    BLI_assert(this->pointer_has_valid_alignment(dst));
+
+    m_copy_to_initialized_indices(src, dst, indices);
+  }
+
   void copy_to_uninitialized(const void *src, void *dst) const
   {
     BLI_assert(this->pointer_has_valid_alignment(src));
@@ -165,6 +219,14 @@ class CPPType {
     m_copy_to_uninitialized_n(src, dst, n);
   }
 
+  void copy_to_uninitialized_indices(const void *src, void *dst, ArrayRef<uint> indices) const
+  {
+    BLI_assert(this->pointer_has_valid_alignment(src));
+    BLI_assert(this->pointer_has_valid_alignment(dst));
+
+    m_copy_to_uninitialized_indices(src, dst, indices);
+  }
+
   void relocate_to_initialized(void *src, void *dst) const
   {
     BLI_assert(this->pointer_has_valid_alignment(src));
@@ -173,12 +235,20 @@ class CPPType {
     m_relocate_to_initialized(src, dst);
   }
 
-  void relocate_to_initialized(void *src, void *dst, uint n) const
+  void relocate_to_initialized_n(void *src, void *dst, uint n) const
   {
     BLI_assert(this->pointer_has_valid_alignment(src));
     BLI_assert(this->pointer_has_valid_alignment(dst));
 
     m_relocate_to_initialized_n(src, dst, n);
+  }
+
+  void relocate_to_initialized_indices(void *src, void *dst, ArrayRef<uint> indices) const
+  {
+    BLI_assert(this->pointer_has_valid_alignment(src));
+    BLI_assert(this->pointer_has_valid_alignment(dst));
+
+    m_relocate_to_initialized_indices(src, dst, indices);
   }
 
   void relocate_to_uninitialized(void *src, void *dst) const
@@ -197,6 +267,14 @@ class CPPType {
     m_relocate_to_uninitialized_n(src, dst, n);
   }
 
+  void relocate_to_uninitialized_indices(void *src, void *dst, ArrayRef<uint> indices) const
+  {
+    BLI_assert(this->pointer_has_valid_alignment(src));
+    BLI_assert(this->pointer_has_valid_alignment(dst));
+
+    m_relocate_to_uninitialized_indices(src, dst, indices);
+  }
+
   void fill_initialized(const void *value, void *dst, uint n) const
   {
     BLI_assert(this->pointer_has_valid_alignment(value));
@@ -205,12 +283,28 @@ class CPPType {
     m_fill_initialized(value, dst, n);
   }
 
+  void fill_initialized_indices(const void *value, void *dst, ArrayRef<uint> indices) const
+  {
+    BLI_assert(this->pointer_has_valid_alignment(value));
+    BLI_assert(this->pointer_has_valid_alignment(dst));
+
+    m_fill_initialized_indices(value, dst, indices);
+  }
+
   void fill_uninitialized(const void *value, void *dst, uint n) const
   {
     BLI_assert(this->pointer_has_valid_alignment(value));
     BLI_assert(this->pointer_has_valid_alignment(dst));
 
     m_fill_uninitialized(value, dst, n);
+  }
+
+  void fill_uninitialized_indices(const void *value, void *dst, ArrayRef<uint> indices) const
+  {
+    BLI_assert(this->pointer_has_valid_alignment(value));
+    BLI_assert(this->pointer_has_valid_alignment(dst));
+
+    m_fill_uninitialized_indices(value, dst, indices);
   }
 
   bool is_same_or_generalization(const CPPType &other) const
@@ -239,20 +333,37 @@ class CPPType {
   uint m_alignment;
   uint m_alignment_mask;
   bool m_trivially_destructible;
+
   ConstructDefaultF m_construct_default;
   ConstructDefaultNF m_construct_default_n;
+  ConstructDefaultIndicesF m_construct_default_indices;
+
   DestructF m_destruct;
   DestructNF m_destruct_n;
+  DestructIndicesF m_destruct_indices;
+
   CopyToInitializedF m_copy_to_initialized;
   CopyToInitializedNF m_copy_to_initialized_n;
+  CopyToInitializedIndicesF m_copy_to_initialized_indices;
+
   CopyToUninitializedF m_copy_to_uninitialized;
   CopyToUninitializedNF m_copy_to_uninitialized_n;
+  CopyToUninitializedIndicesF m_copy_to_uninitialized_indices;
+
   RelocateToInitializedF m_relocate_to_initialized;
   RelocateToInitializedNF m_relocate_to_initialized_n;
+  RelocateToInitializedIndicesF m_relocate_to_initialized_indices;
+
   RelocateToUninitializedF m_relocate_to_uninitialized;
   RelocateToUninitializedNF m_relocate_to_uninitialized_n;
+  RelocateToUninitializedIndicesF m_relocate_to_uninitialized_indices;
+
   FillInitializedF m_fill_initialized;
+  FillInitializedIndicesF m_fill_initialized_indices;
+
   FillUninitializedF m_fill_uninitialized;
+  FillUninitializedIndicesF m_fill_uninitialized_indices;
+
   const CPPType *m_generalization;
   std::string m_name;
 };
--- source/blender/functions/intern/cpp_types.cc
+++ source/blender/functions/intern/cpp_types.cc
@@ -19,10 +19,15 @@ template<typename T> void ConstructDefault_CB(void *ptr)
 {
   BLI::construct_default((T *)ptr);
 }
-
 template<typename T> void ConstructDefaultN_CB(void *ptr, uint n)
 {
   for (uint i = 0; i < n; i++) {
+    BLI::construct_default((T *)ptr + i);
+  }
+}
+template<typename T> void ConstructDefaultIndices_CB(void *ptr, ArrayRef<uint> indices)
+{
+  for (uint i : indices) {
     BLI::construct_default((T *)ptr + i);
   }
 }
@@ -35,6 +40,13 @@ template<typename T> void DestructN_CB(void *ptr, uint n)
 {
   BLI::destruct_n((T *)ptr, n);
 }
+template<typename T> void DestructIndices_CB(void *ptr, ArrayRef<uint> indices)
+{
+  for (uint i : indices) {
+    BLI::destruct((T *)ptr + i);
+  }
+}
+
 template<typename T> void CopyToInitialized_CB(const void *src, void *dst)
 {
   *(T *)dst = *(T *)src;
@@ -48,6 +60,17 @@ template<typename T> void CopyToInitializedN_CB(const void *src, void *dst, uint
     dst_[i] = src_[i];
   }
 }
+template<typename T>
+void CopyToInitializedIndices_CB(const void *src, void *dst, ArrayRef<uint> indices)
+{
+  const T *src_ = (const T *)src;
+  T *dst_ = (T *)dst;
+
+  for (uint i : indices) {
+    dst_[i] = src_[i];
+  }
+}
+
 template<typename T> void CopyToUninitialized_CB(const void *src, void *dst)
 {
   BLI::uninitialized_copy_n((T *)src, 1, (T *)dst);
@@ -56,6 +79,17 @@ template<typename T> void CopyToUninitializedN_CB(const void *src, void *dst, ui
 {
   BLI::uninitialized_copy_n((T *)src, n, (T *)dst);
 }
+template<typename T>
+void CopyToUninitializedIndices_CB(const void *src, void *dst, ArrayRef<uint> indices)
+{
+  const T *src_ = (const T *)src;
+  T *dst_ = (T *)dst;
+
+  for (uint i : indices) {
+    new (dst_ + i) T(src_[i]);
+  }
+}
+
 template<typename T> void RelocateToInitialized_CB(void *src, void *dst)
 {
   BLI::relocate((T *)src, (T *)dst);
@@ -64,6 +98,18 @@ template<typename T> void RelocateToInitializedN_CB(void *src, void *dst, uint n
 {
   BLI::relocate_n((T *)src, n, (T *)dst);
 }
+template<typename T>
+void RelocateToInitializedIndices_CB(void *src, void *dst, ArrayRef<uint> indices)
+{
+  T *src_ = (T *)src;
+  T *dst_ = (T *)dst;
+
+  for (uint i : indices) {
+    dst_[i] = std::move(src_[i]);
+    src_[i].~T();
+  }
+}
+
 template<typename T> void RelocateToUninitialized_CB(void *src, void *dst)
 {
   BLI::uninitialized_relocate((T *)src, (T *)dst);
@@ -72,6 +118,18 @@ template<typename T> void RelocateToUninitializedN_CB(void *src, void *dst, uint
 {
   BLI::uninitialized_relocate_n((T *)src, n, (T *)dst);
 }
+template<typename T>
+void RelocateToUninitializedIndices_CB(void *src, void *dst, ArrayRef<uint> indices)
+{
+  T *src_ = (T *)src;
+  T *dst_ = (T *)dst;
+
+  for (uint i : indices) {
+    new (dst_ + i) T(std::move(src_[i]));
+    src_[i].~T();
+  }
+}
+
 template<typename T> void FillInitialized_CB(const void *value, void *dst, uint n)
 {
   const T &value_ = *(const T *)value;
@@ -81,12 +139,33 @@ template<typename T> void FillInitialized_CB(const void *value, void *dst, uint 
     dst_[i] = value_;
   }
 }
+template<typename T>
+void FillInitializedIndices_CB(const void *value, void *dst, ArrayRef<uint> indices)
+{
+  const T &value_ = *(const T *)value;
+  T *dst_ = (T *)dst;
+
+  for (uint i : indices) {
+    dst_[i] = value_;
+  }
+}
+
 template<typename T> void FillUninitialized_CB(const void *value, void *dst, uint n)
 {
   const T &value_ = *(const T *)value;
   T *dst_ = (T *)dst;
 
   for (uint i = 0; i < n; i++) {
+    new (dst_ + i) T(value_);
+  }
+}
+template<typename T>
+void FillUninitializedIndices_CB(const void *value, void *dst, ArrayRef<uint> indices)
+{
+  const T &value_ = *(const T *)value;
+  T *dst_ = (T *)dst;
+
+  for (uint i : indices) {
     new (dst_ + i) T(value_);
   }
 }
@@ -99,18 +178,26 @@ template<typename T> static std::unique_ptr<const CPPType> create_cpp_type(Strin
                                     std::is_trivially_destructible<T>::value,
                                     ConstructDefault_CB<T>,
                                     ConstructDefaultN_CB<T>,
+                                    ConstructDefaultIndices_CB<T>,
                                     Destruct_CB<T>,
                                     DestructN_CB<T>,
+                                    DestructIndices_CB<T>,
                                     CopyToInitialized_CB<T>,
                                     CopyToInitializedN_CB<T>,
+                                    CopyToInitializedIndices_CB<T>,
                                     CopyToUninitialized_CB<T>,
                                     CopyToUninitializedN_CB<T>,
+                                    CopyToUninitializedIndices_CB<T>,
                                     RelocateToInitialized_CB<T>,
                                     RelocateToInitializedN_CB<T>,
+                                    RelocateToInitializedIndices_CB<T>,
                                     RelocateToUninitialized_CB<T>,
                                     RelocateToUninitializedN_CB<T>,
+                                    RelocateToUninitializedIndices_CB<T>,
                                     FillInitialized_CB<T>,
+                                    FillInitializedIndices_CB<T>,
                                     FillUninitialized_CB<T>,
+                                    FillUninitializedIndices_CB<T>,
                                     nullptr);
   return std::unique_ptr<const CPPType>(type);
 }
