787f6c8a7e3c2e1fb701798df3753e7bd77d4179
Jacques Lucke <mail@jlucke.com>
2020-01-27 22:05:55 +0100

    enable node tree optimizations

--- source/blender/functions/intern/node_tree_multi_function_network/generate.cc
+++ source/blender/functions/intern/node_tree_multi_function_network/generate.cc
@@ -270,9 +270,9 @@ std::unique_ptr<FunctionTreeMFNetwork> generate_node_tree_multi_function_network
     BLI_assert(false);
   }
 
-  // optimize_network__constant_folding(network_builder, resources);
-  // optimize_network__remove_duplicates(network_builder);
-  // optimize_network__remove_unused_nodes(network_builder);
+  optimize_network__constant_folding(network_builder, resources);
+  optimize_network__remove_duplicates(network_builder);
+  optimize_network__remove_unused_nodes(network_builder);
   // network_builder.to_dot__clipboard();
   // function_tree.to_dot__clipboard();
   auto function_tree_network = build(function_tree, network_builder, dummy_socket_mapping);
