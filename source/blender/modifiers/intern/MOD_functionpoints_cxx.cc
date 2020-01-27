166c7fbd60440539679183013f40729f7b54bb4c
Jacques Lucke <mail@jlucke.com>
2020-01-27 23:29:13 +0100

    initial performance test

--- source/blender/modifiers/intern/MOD_functionpoints_cxx.cc
+++ source/blender/modifiers/intern/MOD_functionpoints_cxx.cc
@@ -7,6 +7,8 @@
 #include "BKE_id_data_cache.h"
 
 #include "BLI_math.h"
+#include "BLI_array.h"
+#include "BLI_timeit.h"
 
 #include "FN_node_tree_multi_function_network_generation.h"
 #include "FN_multi_functions.h"
@@ -17,9 +19,11 @@
 #include "DEG_depsgraph_query.h"
 
 using BKE::VNode;
+using BLI::Array;
 using BLI::ArrayRef;
 using BLI::float3;
 using BLI::IndexRange;
+using BLI::MutableArrayRef;
 using BLI::Vector;
 using FN::FunctionTree;
 using FN::MFContext;
@@ -32,6 +36,31 @@ Mesh *MOD_functionpoints_do(FunctionPointsModifierData *fpmd,
                             const struct ModifierEvalContext *ctx);
 }
 
+BLI_NOINLINE static void compute_triangle_areas(ArrayRef<float3> points_a,
+                                                ArrayRef<float3> points_b,
+                                                ArrayRef<float3> points_c,
+                                                MutableArrayRef<float> r_areas)
+{
+  uint amount = points_a.size();
+  BLI_assert(points_b.size() == amount);
+  BLI_assert(points_c.size() == amount);
+  BLI_assert(r_areas.size() == amount);
+
+  for (uint i = 0; i < amount; i++) {
+    float3 a = points_a[i];
+    float3 b = points_b[i];
+    float3 c = points_c[i];
+
+    float side1 = float3::distance(a, b);
+    float side2 = float3::distance(a, c);
+    float side3 = float3::distance(b, c);
+
+    float s = (side1 + side2 + side3) * 0.5f;
+    float area = sqrtf(s * (s - side1) * (s - side2) * (s - side3));
+    r_areas[i] = area;
+  }
+}
+
 Mesh *MOD_functionpoints_do(FunctionPointsModifierData *fpmd,
                             const struct ModifierEvalContext *ctx)
 {
@@ -41,11 +70,50 @@ Mesh *MOD_functionpoints_do(FunctionPointsModifierData *fpmd,
 
   bNodeTree *btree = (bNodeTree *)DEG_get_original_id((ID *)fpmd->function_tree);
 
-  FN::BTreeVTreeMap vtrees;
-  FunctionTree function_tree(btree, vtrees);
-
   BLI::ResourceCollector resources;
-  auto function = FN::MFGeneration::generate_node_tree_multi_function(function_tree, resources);
+  std::unique_ptr<const FN::MultiFunction> function;
+  {
+    SCOPED_TIMER("generate function from node tree")
+    FN::BTreeVTreeMap vtrees;
+    FunctionTree function_tree(btree, vtrees);
+    function = FN::MFGeneration::generate_node_tree_multi_function(function_tree, resources);
+  }
+
+  MFParamsBuilder params_builder(*function, 1);
+  FN::MFContextBuilder context_builder;
+
+  uint amount = 10000000;
+  Array<float3> points_a(amount);
+  Array<float3> points_b(amount);
+  Array<float3> points_c(amount);
+  Array<float> results1(amount, 0.0f);
+  Array<float> results2(amount, 0.0f);
+
+  for (uint i : IndexRange(amount)) {
+    points_a[i] = {1, 2, 3};
+    points_b[i] = {5, 3, 2};
+    points_c[i] = {3, 5, 7};
+  }
+
+  params_builder.add_readonly_single_input(points_a.as_ref());
+  params_builder.add_readonly_single_input(points_b.as_ref());
+  params_builder.add_readonly_single_input(points_c.as_ref());
+  params_builder.add_single_output(results1.as_mutable_ref());
+
+  {
+    SCOPED_TIMER("Node Tree");
+    function->call(IndexRange(amount), params_builder, context_builder);
+  }
+  std::cout << "Area 1: " << results1[0] << "\n";
+
+  {
+    SCOPED_TIMER("C++");
+    compute_triangle_areas(points_a, points_b, points_c, results2);
+  }
+  std::cout << "Area 2: " << results2[0] << "\n";
+
+  return BKE_mesh_new_nomain(0, 0, 0, 0, 0);
+  /*
 
   MFParamsBuilder params_builder(*function, 1);
   params_builder.add_readonly_single_input(&fpmd->control1);
@@ -77,4 +145,5 @@ Mesh *MOD_functionpoints_do(FunctionPointsModifierData *fpmd,
   }
 
   return mesh;
+  */
 }
