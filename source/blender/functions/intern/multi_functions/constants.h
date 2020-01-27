3a27fb824eaab5371f7dcfd6f6ee759c5195a264
Jacques Lucke <mail@jlucke.com>
2020-01-27 21:57:50 +0100

    fix typo

--- source/blender/functions/intern/multi_functions/constants.h
+++ source/blender/functions/intern/multi_functions/constants.h
@@ -68,8 +68,8 @@ template<typename T> class MF_ConstantValue : public MultiFunction {
     else if (CPP_TYPE<T>() == CPP_TYPE<BLI::float3>()) {
       BLI::float3 vector = *(BLI::float3 *)&value;
       uint32_t hash = BLI_hash_int_2d(*(uint *)&vector.x, 0);
-      hash = BLI_hash_int_2d(*(uint *)&vector.x, hash);
-      hash = BLI_hash_int_2d(*(uint *)&vector.x, hash);
+      hash = BLI_hash_int_2d(*(uint *)&vector.y, hash);
+      hash = BLI_hash_int_2d(*(uint *)&vector.z, hash);
       signature.operation_hash(hash);
     }
   }
