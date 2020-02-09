2a440baca135554d46706c3a736a9723e95f9466
Jacques Lucke <mail@jlucke.com>
2020-02-09 16:07:56 +0100

    change how identifiers are recognized

--- source/blender/functions/intern/expression_lexer.cc
+++ source/blender/functions/intern/expression_lexer.cc
@@ -186,13 +186,64 @@ void tokenize(StringRef str,
                      });
         break;
       }
+      case '_':
+      case 'a':
+      case 'b':
+      case 'c':
+      case 'd':
+      case 'e':
+      case 'f':
+      case 'g':
+      case 'h':
+      case 'i':
+      case 'j':
+      case 'k':
+      case 'l':
+      case 'm':
+      case 'n':
+      case 'o':
+      case 'p':
+      case 'q':
+      case 'r':
+      case 's':
+      case 't':
+      case 'u':
+      case 'v':
+      case 'w':
+      case 'x':
+      case 'y':
+      case 'z':
+      case 'A':
+      case 'B':
+      case 'C':
+      case 'D':
+      case 'E':
+      case 'F':
+      case 'G':
+      case 'H':
+      case 'I':
+      case 'J':
+      case 'K':
+      case 'L':
+      case 'M':
+      case 'N':
+      case 'O':
+      case 'P':
+      case 'Q':
+      case 'R':
+      case 'S':
+      case 'T':
+      case 'U':
+      case 'V':
+      case 'W':
+      case 'X':
+      case 'Y':
+      case 'Z': {
+        tokenize_identifier(str.drop_prefix(offset), token_size, token_type);
+        break;
+      }
       default: {
-        if (is_identifier_start(current_char)) {
-          tokenize_identifier(str.drop_prefix(offset), token_size, token_type);
-        }
-        else {
-          BLI_assert(false);
-        }
+        BLI_assert(false);
         break;
       }
     }
