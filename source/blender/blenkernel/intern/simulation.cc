9973f752b4177f90764ad3e7cef8d26dd233132f
Jacques Lucke <jacques@blender.org>
2020-04-27 13:35:15 +0200

    quiet warning

--- source/blender/blenkernel/intern/simulation.cc
+++ source/blender/blenkernel/intern/simulation.cc
@@ -112,7 +112,7 @@ IDTypeInfo IDType_ID_SIM = {
     /* make_local */ nullptr,
 };
 
-void BKE_simulation_data_update(Depsgraph *depsgraph, Scene *scene)
+void BKE_simulation_data_update(Depsgraph *UNUSED(depsgraph), Scene *scene)
 {
   printf("Eval simulation: %d + %f\n", scene->r.cfra, scene->r.subframe);
 }
