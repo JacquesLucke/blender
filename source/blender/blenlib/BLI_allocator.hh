e07c1dd4986c930529f6b97946fd59e97a05284d
Jacques Lucke <jacques@blender.org>
2020-06-04 13:25:11 +0200

    allocator docs

--- source/blender/blenlib/BLI_allocator.hh
+++ source/blender/blenlib/BLI_allocator.hh
@@ -20,6 +20,8 @@
  * \ingroup bli
  *
  * An `Allocator` can allocate and deallocate memory. It is used by containers such as BLI::Vector.
+ * The allocators defined in this file do not work with standard library containers such as
+ * std::Vector.
  *
  * Every allocator has to implement two methods:
  *   void *allocate(size_t size, size_t alignment, const char *name);
@@ -28,10 +30,12 @@
  * We don't use the std::allocator interface, because it does more than is really necessary for an
  * allocator and has some other quirks. It mixes the concepts of allocation and construction. It is
  * essentially forced to be a template, even though the allocator should not care about the type.
- * Also see http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2007/n2271.html#std_allocator.
+ * Also see http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2007/n2271.html#std_allocator. Some
+ * of these aspects have been improved in new versions of C++, so we might have to reevaluate the
+ * strategy later on.
  *
  * The allocator interface dictated by this file is very simplistic, but for now that is all we
- * need. More complexity can be added later when it seems necessary.
+ * need. More complexity can be added when it seems necessary.
  */
 
 #include <algorithm>
@@ -45,7 +49,7 @@
 namespace BLI {
 
 /**
- * Use Blenders guarded allocator (aka MEM_malloc). This should always be used except there is a
+ * Use Blender's guarded allocator (aka MEM_*). This should always be used except there is a
  * good reason not to use it.
  */
 class GuardedAllocator {
@@ -65,7 +69,7 @@ class GuardedAllocator {
 /**
  * This is a wrapper around malloc/free. Only use this when the GuardedAllocator cannot be
  * used. This can be the case when the allocated memory might live longer than Blender's
- * allocator. For example, when it is owned by static variables.
+ * allocator. For example, when the memory is owned by a static variable.
  */
 class RawAllocator {
  private:
