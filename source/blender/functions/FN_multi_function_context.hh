0cfbd5c93d365d8f89444b6651d1cd8b5a497d3e
Jacques Lucke <jacques@blender.org>
2020-06-15 10:12:56 +0200

    add MFContext

---
+++ source/blender/functions/FN_multi_function_context.hh
@@ -0,0 +1,39 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __FN_MULTI_FUNCTION_CONTEXT_HH__
+#define __FN_MULTI_FUNCTION_CONTEXT_HH__
+
+namespace blender {
+namespace FN {
+
+class MFContextBuilder {
+};
+
+class MFContext {
+ private:
+  MFContextBuilder *m_builder;
+
+ public:
+  MFContext(MFContextBuilder &builder) : m_builder(&builder)
+  {
+  }
+};
+
+}  // namespace FN
+}  // namespace blender
+
+#endif /* __FN_MULTI_FUNCTION_CONTEXT_HH__ */
