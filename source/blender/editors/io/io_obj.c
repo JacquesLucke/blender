c39128ca97bf0f13209f019d7e38ecafc9791bde
Ankit Meel <ankitjmeel@gmail.com>
2020-06-28 02:46:20 +0530

    UI: show warning when overwriting an export file

--- source/blender/editors/io/io_obj.c
+++ source/blender/editors/io/io_obj.c
@@ -232,7 +232,7 @@ void WM_OT_obj_export(struct wmOperatorType *ot)
   WM_operator_properties_filesel(ot,
                                  FILE_TYPE_FOLDER | FILE_TYPE_OBJECT_IO,
                                  FILE_BLENDER,
-                                 FILE_OPENFILE,
+                                 FILE_SAVE,
                                  WM_FILESEL_FILEPATH | WM_FILESEL_SHOW_PROPS,
                                  FILE_DEFAULTDISPLAY,
                                  FILE_SORT_ALPHA);
