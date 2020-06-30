d3012abe1300941c9a916b232d7b6bc19754b9e3
Jacques Lucke <jacques@blender.org>
2020-06-30 15:14:28 +0200

    make combine strings node work

--- source/blender/nodes/function/nodes/node_fn_combine_strings.cc
+++ source/blender/nodes/function/nodes/node_fn_combine_strings.cc
@@ -1,4 +1,11 @@
+#include "FN_multi_function_builder.hh"
+
+#include "BKE_node_tree_function.hh"
+
 #include "node_function_util.h"
+
+namespace blender {
+namespace node {
 
 static bNodeSocketTemplate fn_node_combine_strings_in[] = {
     {SOCK_STRING, N_("A")},
@@ -11,11 +18,24 @@ static bNodeSocketTemplate fn_node_combine_strings_out[] = {
     {-1, ""},
 };
 
+static void fn_node_combine_strings_build_mf_network(bke::NodeMFNetworkBuilder &builder)
+{
+  static fn::CustomMF_SI_SI_SO<std::string, std::string, std::string> combine_fn{
+      "Combine Strings", [](const std::string &a, const std::string &b) { return a + b; }};
+  builder.set_matching_fn(combine_fn);
+}
+
+}  // namespace node
+}  // namespace blender
+
 void register_node_type_fn_combine_strings()
 {
   static bNodeType ntype;
 
   fn_node_type_base(&ntype, FN_NODE_COMBINE_STRINGS, "Combine Strings", 0, 0);
-  node_type_socket_templates(&ntype, fn_node_combine_strings_in, fn_node_combine_strings_out);
+  node_type_socket_templates(&ntype,
+                             blender::node::fn_node_combine_strings_in,
+                             blender::node::fn_node_combine_strings_out);
+  ntype.build_mf_network = blender::node::fn_node_combine_strings_build_mf_network;
   nodeRegisterType(&ntype);
 }
