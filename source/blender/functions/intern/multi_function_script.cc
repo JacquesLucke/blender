e95cdf26d6e2ac31e0e4b4d67122bc000593dd5b
Jacques Lucke <jacques@blender.org>
2020-10-31 11:50:37 +0100

    initial branching

--- source/blender/functions/intern/multi_function_script.cc
+++ source/blender/functions/intern/multi_function_script.cc
@@ -33,6 +33,10 @@ MFScriptEvaluator::MFScriptEvaluator(const MFScript &script) : script_(script)
 
 void MFScriptEvaluator::call(IndexMask mask, MFParams params, MFContext context) const
 {
+  if (script_.entry == nullptr) {
+    return;
+  }
+
   const int array_size = mask.min_array_size();
   GuardedAllocator allocator;
 
@@ -71,39 +75,75 @@ void MFScriptEvaluator::call(IndexMask mask, MFParams params, MFContext context)
     buffers_to_destruct_and_free.append(span);
   }
 
-  const MFInstruction *current_instruction = script_.entry;
-  while (current_instruction != nullptr) {
-    BLI_assert(current_instruction->type == MFInstructionType::Call);
-    const MFCallInstruction &instruction = *static_cast<const MFCallInstruction *>(
-        current_instruction);
+  Map<const MFInstruction *, Vector<Vector<int64_t>>> masks_by_instruction;
+  masks_by_instruction.add_new(script_.entry, {mask.indices()});
 
-    const MultiFunction &fn = *instruction.function;
-    MFParamsBuilder params{fn, array_size};
-    for (const int param_index : fn.param_indices()) {
-      MFParamType param_type = fn.param_type(param_index);
-      BLI_assert(param_type.data_type().is_single());
-      const MFRegister *reg = instruction.registers[param_index];
-      GMutableSpan span = register_buffers.lookup(reg);
-      switch (param_type.category()) {
-        case MFParamType::Category::SingleInput: {
-          params.add_readonly_single_input(span);
-          break;
+  while (!masks_by_instruction.is_empty()) {
+    const MFInstruction *current_instr = *masks_by_instruction.keys().begin();
+    Vector<Vector<int64_t>> masks = masks_by_instruction.pop(current_instr);
+    switch (current_instr->type) {
+      case MFInstructionType::Call: {
+        const MFCallInstruction &instr = *static_cast<const MFCallInstruction *>(current_instr);
+        for (Span<int64_t> sub_mask : masks) {
+          const MultiFunction &fn = *instr.function;
+          MFParamsBuilder params{fn, array_size};
+          for (const int param_index : fn.param_indices()) {
+            MFParamType param_type = fn.param_type(param_index);
+            BLI_assert(param_type.data_type().is_single());
+            const MFRegister *reg = instr.registers[param_index];
+            GMutableSpan span = register_buffers.lookup(reg);
+            switch (param_type.category()) {
+              case MFParamType::Category::SingleInput: {
+                params.add_readonly_single_input(span);
+                break;
+              }
+              case MFParamType::Category::SingleOutput: {
+                const CPPType &type = span.type();
+                type.destruct_indices(span.data(), mask);
+                params.add_uninitialized_single_output(span);
+                break;
+              }
+              default: {
+                BLI_assert(false);
+              }
+            }
+          }
+
+          fn.call(sub_mask, params, context);
         }
-        case MFParamType::Category::SingleOutput: {
-          const CPPType &type = span.type();
-          type.destruct_indices(span.data(), mask);
-          params.add_uninitialized_single_output(span);
-          break;
+        if (instr.next != nullptr) {
+          for (Vector<int64_t> &sub_mask : masks) {
+            masks_by_instruction.lookup_or_add_default(instr.next).append(std::move(sub_mask));
+          }
         }
-        default: {
-          BLI_assert(false);
+        break;
+      }
+      case MFInstructionType::Branch: {
+        const MFBranchInstruction &instr = *static_cast<const MFBranchInstruction *>(
+            current_instr);
+        Span<bool> condition = register_buffers.lookup(instr.condition).typed<bool>();
+        for (Span<int64_t> sub_mask : masks) {
+          Vector<int64_t> true_mask, false_mask;
+          for (int64_t i : sub_mask) {
+            if (condition[i]) {
+              true_mask.append(i);
+            }
+            else {
+              false_mask.append(i);
+            }
+          }
+          if (instr.true_instruction != nullptr) {
+            masks_by_instruction.lookup_or_add_default(instr.true_instruction)
+                .append(std::move(true_mask));
+          }
+          if (instr.false_instruction != nullptr) {
+            masks_by_instruction.lookup_or_add_default(instr.false_instruction)
+                .append(std::move(false_mask));
+          }
         }
+        break;
       }
     }
-
-    fn.call(mask, params, context);
-
-    current_instruction = instruction.next;
   }
 
   for (GMutableSpan span : buffers_to_destruct_and_free) {
--- source/blender/functions/tests/FN_multi_function_script_test.cc
+++ source/blender/functions/tests/FN_multi_function_script_test.cc
@@ -9,41 +9,55 @@ namespace blender::fn::script::tests {
 
 TEST(multi_function_script, Test1)
 {
-  CustomMF_SI_SO<int, int> add_10_fn("add 20", [](int value) { return value + 20; });
+  CustomMF_SI_SO<int, int> add_10_fn("add 10", [](int value) { return value + 10; });
+  CustomMF_SI_SO<int, int> add_50_fn("add 50", [](int value) { return value + 50; });
+  CustomMF_SI_SO<int, bool> smaller_than_20_fn("smaller than 20",
+                                               [](int value) { return value < 20; });
 
   MFRegister reg1{MFDataType::ForSingle<int>()};
   MFRegister reg2{MFDataType::ForSingle<int>()};
-  MFRegister reg3{MFDataType::ForSingle<int>()};
+  MFRegister reg3{MFDataType::ForSingle<bool>()};
 
-  MFCallInstruction instr1;
-  instr1.function = &add_10_fn;
-  instr1.registers = {&reg1, &reg2};
+  MFCallInstruction compare_instr;
+  compare_instr.function = &smaller_than_20_fn;
+  compare_instr.registers = {&reg1, &reg3};
 
-  MFCallInstruction instr2;
-  instr2.function = &add_10_fn;
-  instr2.registers = {&reg2, &reg3};
+  MFBranchInstruction branch_instr;
+  branch_instr.condition = &reg3;
 
-  instr1.next = &instr2;
+  MFCallInstruction add_10_instr;
+  add_10_instr.function = &add_10_fn;
+  add_10_instr.registers = {&reg1, &reg2};
+
+  MFCallInstruction add_50_instr;
+  add_50_instr.function = &add_50_fn;
+  add_50_instr.registers = {&reg1, &reg2};
+
+  compare_instr.next = &branch_instr;
+  branch_instr.true_instruction = &add_10_instr;
+  branch_instr.false_instruction = &add_50_instr;
 
   MFScript script;
-  script.entry = &instr1;
+  script.entry = &compare_instr;
   script.registers = {&reg1, &reg2, &reg3};
   script.input_registers = {&reg1};
-  script.output_registers = {&reg3};
+  script.output_registers = {&reg2};
 
   MFScriptEvaluator script_fn{script};
 
-  MFParamsBuilder params{script_fn, 1};
+  Array<int> input_values = {16, 17, 18, 19, 20, 21, 20};
+  Array<int> output_values(input_values.size());
+
+  MFParamsBuilder params{script_fn, input_values.size()};
   MFContextBuilder context;
+  params.add_readonly_single_input(input_values.as_span());
+  params.add_uninitialized_single_output(output_values.as_mutable_span());
 
-  int input_value = 13;
-  int output_value;
-  params.add_readonly_single_input(&input_value);
-  params.add_uninitialized_single_output(&output_value);
+  script_fn.call(input_values.index_range(), params, context);
 
-  script_fn.call({0}, params, context);
-
-  EXPECT_EQ(output_value, 53);
+  for (int value : output_values) {
+    std::cout << value << "\n";
+  }
 }
 
 }  // namespace blender::fn::script::tests
