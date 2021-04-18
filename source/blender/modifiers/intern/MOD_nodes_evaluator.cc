a072f8a36f1e8e7c1919f1994202ffb31f634f92
Jacques Lucke <jacques@blender.org>
2021-04-18 12:37:33 +0200

    support disabling input socket

--- source/blender/modifiers/intern/MOD_nodes_evaluator.cc
+++ source/blender/modifiers/intern/MOD_nodes_evaluator.cc
@@ -45,7 +45,13 @@ class Evaluator {
   {
     this->initialize_node_states();
     this->initialize_socket_users();
-    node_states_.print_stats();
+
+    const DerivedNodeTree &tree = params_.output_sockets[0].context()->derived_tree();
+    tree.foreach_node([&, this](const DNode node) {
+      if (node->name() == "Join Geometry") {
+        this->disable_input_socket(DInputSocket{node.context(), &node->input(0)});
+      }
+    });
 
     auto get_socket_users = [&, this](const DSocket socket) -> std::string {
       if (socket->is_input()) {
@@ -56,12 +62,11 @@ class Evaluator {
       if (node_state == nullptr) {
         return " 0";
       }
-      return std::to_string(node_state->outputs[socket->index()].users.size());
+      return " " + std::to_string(node_state->outputs[socket->index()].users.size());
     };
 
     DerivedNodeTree::ToDotParams to_dot_params;
     to_dot_params.get_additional_socket_label = get_socket_users;
-    const DerivedNodeTree &tree = params_.output_sockets[0].context()->derived_tree();
     std::cout << "\n\n" << tree.to_dot(to_dot_params) << "\n\n";
 
     this->destruct_node_states();
@@ -120,6 +125,50 @@ class Evaluator {
             {});
       }
     }
+  }
+
+  void disable_input_socket(const DInputSocket socket_to_disable)
+  {
+    Stack<DNode> nodes_to_check;
+    this->disable_input_socket_direct(socket_to_disable, nodes_to_check);
+
+    while (!nodes_to_check.is_empty()) {
+      const DNode node = nodes_to_check.pop();
+      NodeState &node_state = node_states_.lookup(node);
+      bool any_output_has_users = false;
+      for (const OutputSocketState &socket_state : node_state.outputs) {
+        if (!socket_state.users.is_empty()) {
+          any_output_has_users = true;
+        }
+      }
+      if (any_output_has_users) {
+        continue;
+      }
+      for (const InputSocketRef *input_socket_ref : node->inputs()) {
+        const DInputSocket input_socket{node.context(), input_socket_ref};
+        this->disable_input_socket_direct(input_socket, nodes_to_check);
+      }
+    }
+  }
+
+  void disable_input_socket_direct(const DInputSocket socket_to_disable,
+                                   Stack<DNode> &nodes_to_check)
+  {
+    socket_to_disable.foreach_origin_socket([&, this](const DSocket origin_socket) {
+      if (origin_socket->is_input()) {
+        return;
+      }
+      const DNode origin_node = origin_socket.node();
+      NodeState &node_state = this->node_states_.lookup(origin_node);
+      OutputSocketState &socket_state = node_state.outputs[origin_socket->index()];
+      const int index_to_remove = socket_state.users.first_index_of_try(socket_to_disable);
+      if (index_to_remove == -1) {
+        /* Has been removed before. */
+        return;
+      }
+      socket_state.users.remove_and_reorder(index_to_remove);
+      nodes_to_check.push(origin_node);
+    });
   }
 };
 }  // namespace
