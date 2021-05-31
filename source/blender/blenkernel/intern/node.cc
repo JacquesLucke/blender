7694a098efb5abb3e81e8b2b71d915764683bd1a
Jacques Lucke <jacques@blender.org>
2021-05-31 12:46:39 +0200

    initial viewer node

--- source/blender/blenkernel/intern/node.cc
+++ source/blender/blenkernel/intern/node.cc
@@ -5081,6 +5081,7 @@ static void registerGeometryNodes()
   register_node_type_geo_switch();
   register_node_type_geo_transform();
   register_node_type_geo_triangulate();
+  register_node_type_geo_viewer();
   register_node_type_geo_volume_to_mesh();
 }
 
