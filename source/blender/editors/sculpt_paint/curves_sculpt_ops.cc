47b011e7e9fd0f1fa01842265644a73fc25919bb
Jacques Lucke <jacques@blender.org>
2022-02-18 11:54:50 +0100

    support inserting new curves

--- source/blender/editors/sculpt_paint/curves_sculpt_ops.cc
+++ source/blender/editors/sculpt_paint/curves_sculpt_ops.cc
@@ -1,6 +1,9 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include "BLI_float4x4.hh"
+#include "BLI_math_vector.hh"
 #include "BLI_utildefines.h"
+#include "BLI_vector.hh"
 
 #include "BKE_context.h"
 #include "BKE_paint.h"
@@ -8,13 +11,17 @@
 #include "WM_api.h"
 
 #include "ED_curves_sculpt.h"
+#include "ED_screen.h"
+#include "ED_view3d.h"
 
+#include "BKE_curves.hh"
 #include "BKE_curves_sculpt.hh"
 
-#include "BLI_math_vector.hh"
-#include "BLI_vector.hh"
-
 #include "RNA_access.h"
+
+#include "DNA_curves_types.h"
+
+#include "DEG_depsgraph.h"
 
 #include "curves_sculpt_intern.h"
 #include "paint_intern.h"
@@ -27,6 +34,7 @@ bool CURVES_SCULPT_mode_poll(struct bContext *C)
 
 namespace blender::ed::curves_sculpt {
 
+using bke::CurvesGeometry;
 using bke::CurvesSculptSession;
 using bke::CurvesSculptStroke;
 
@@ -49,18 +57,40 @@ static void stroke_update_step(bContext *C,
                                PaintStroke *UNUSED(paint_stroke),
                                PointerRNA *stroke_element)
 {
+  Depsgraph *depsgraph = CTX_data_depsgraph_pointer(C);
   Object *object = CTX_data_active_object(C);
   CurvesSculptSession &session = bke::curves_sculpt_session_ensure(*object);
   if (!session.current_stroke.has_value()) {
     session.current_stroke.emplace();
   }
 
+  Curves &curves_id = *static_cast<Curves *>(object->data);
+  CurvesGeometry &curves = CurvesGeometry::wrap(curves_id.geometry);
+
   CurvesSculptStroke &stroke = *session.current_stroke;
 
   float2 mouse_position;
   RNA_float_get_array(stroke_element, "mouse", mouse_position);
   stroke.mouse_positions.append(mouse_position);
-  stroke.mouse_positions.as_span().print_as_lines("Mouse Positions");
+
+  ViewContext view_context;
+  ED_view3d_viewcontext_init(C, &view_context, depsgraph);
+
+  float3 ray_start, ray_end;
+  ED_view3d_win_to_segment_clipped(
+      depsgraph, view_context.region, view_context.v3d, mouse_position, ray_start, ray_end, true);
+  const float3 ray_direction = math::normalize(ray_end - ray_start);
+  std::cout << ray_start << " -> " << ray_end << "\n";
+
+  curves.resize(curves.point_size + 2, curves.curve_size + 1);
+  MutableSpan<float3> positions = curves.positions().take_back(2);
+  positions[0] = ray_start + ray_direction * 4;
+  positions[1] = ray_start + ray_direction * 6;
+  MutableSpan<int> offsets = curves.offsets();
+  offsets.last() = curves.point_size;
+
+  DEG_id_tag_update(&curves_id.id, ID_RECALC_GEOMETRY);
+  ED_region_tag_redraw(view_context.region);
 }
 
 static void stroke_done(const bContext *C, PaintStroke *UNUSED(stroke))
