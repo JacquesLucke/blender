18c7e9ad01830625f25767fb3b3c5c6479e15a29
Jacques Lucke <jacques@blender.org>
2022-02-21 11:22:07 +0100

    cleanup

--- source/blender/editors/sculpt_paint/curves_sculpt_ops.cc
+++ source/blender/editors/sculpt_paint/curves_sculpt_ops.cc
@@ -10,8 +10,10 @@
 #include "BKE_paint.h"
 
 #include "WM_api.h"
+#include "WM_toolsystem.h"
 
 #include "ED_curves_sculpt.h"
+#include "ED_object.h"
 #include "ED_screen.h"
 #include "ED_view3d.h"
 
@@ -184,10 +186,65 @@ static void SCULPT_CURVES_OT_brush_stroke(struct wmOperatorType *ot)
   paint_stroke_operator_properties(ot);
 }
 
+static bool curves_sculptmode_toggle_poll(bContext *C)
+{
+  Object *ob = CTX_data_active_object(C);
+  if (ob == nullptr) {
+    return false;
+  }
+  if (ob->type != OB_CURVES) {
+    return false;
+  }
+  return true;
+}
+
+static int curves_sculptmode_toggle_exec(bContext *C, wmOperator *op)
+{
+  Scene *scene = CTX_data_scene(C);
+  Object *ob = CTX_data_active_object(C);
+  const bool is_mode_set = ob->mode == OB_MODE_SCULPT_CURVES;
+
+  if (is_mode_set) {
+    if (!ED_object_mode_compat_set(C, ob, OB_MODE_SCULPT_CURVES, op->reports)) {
+      return OPERATOR_CANCELLED;
+    }
+  }
+
+  if (is_mode_set) {
+    ob->mode = OB_MODE_OBJECT;
+    WM_paint_cursor_end(
+        static_cast<wmPaintCursor *>(scene->toolsettings->curves_sculpt->paint.paint_cursor));
+    scene->toolsettings->curves_sculpt->paint.paint_cursor = nullptr;
+  }
+  else {
+    BKE_paint_ensure(scene->toolsettings, (Paint **)&scene->toolsettings->curves_sculpt);
+    ob->mode = OB_MODE_SCULPT_CURVES;
+
+    paint_cursor_start(&scene->toolsettings->curves_sculpt->paint, nullptr);
+  }
+
+  WM_toolsystem_update_from_context_view3d(C);
+  WM_event_add_notifier(C, NC_SCENE | ND_MODE, nullptr);
+  return OPERATOR_CANCELLED;
+}
+
+static void CURVES_OT_sculptmode_toggle(wmOperatorType *ot)
+{
+  ot->name = "Curve Sculpt Mode Toggle";
+  ot->idname = "CURVES_OT_sculptmode_toggle";
+  ot->description = "Enter/Exit sculpt mode for curves";
+
+  ot->exec = curves_sculptmode_toggle_exec;
+  ot->poll = curves_sculptmode_toggle_poll;
+
+  ot->flag = OPTYPE_UNDO | OPTYPE_REGISTER;
+}
+
 }  // namespace blender::ed::curves_sculpt
 
 void ED_operatortypes_sculpt_curves()
 {
   using namespace blender::ed::curves_sculpt;
   WM_operatortype_append(SCULPT_CURVES_OT_brush_stroke);
+  WM_operatortype_append(CURVES_OT_sculptmode_toggle);
 }
