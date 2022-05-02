ff6fd77ab229f9b0b614f57a28e4e981dff37f02
Jacques Lucke <jacques@blender.org>
2022-05-02 12:53:53 +0200

    progress

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -532,6 +532,10 @@ class _draw_tool_settings_context_mode:
         if brush.curves_sculpt_tool == 'DELETE':
             layout.prop(brush, "falloff_shape", expand=True)
 
+        if brush.curves_sculpt_tool == 'PUFF':
+            layout.prop(brush, "falloff_shape", expand=True)
+
+
 
 class VIEW3D_HT_header(Header):
     bl_space_type = 'VIEW_3D'
@@ -5687,7 +5691,7 @@ class VIEW3D_PT_object_type_visibility(Panel):
     bl_label = "View Object Types"
     bl_ui_units_x = 7
 
-    # Allows derived classes to pass view data other than context.space_data. 
+    # Allows derived classes to pass view data other than context.space_data.
     # This is used by the official VR add-on, which passes XrSessionSettings
     # since VR has a 3D view that only exists for the duration of the VR session.
     def draw_ex(self, context, view, show_select):
