fe477b05d794da567bd3e8039684d94b9284c097
Jacques Lucke <jacques@blender.org>
2022-06-03 15:52:54 +0200

    progress

--- source/blender/editors/sculpt_paint/curves_sculpt_slide.cc
+++ source/blender/editors/sculpt_paint/curves_sculpt_slide.cc
@@ -1,12 +1,85 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include <algorithm>
+
 #include "curves_sculpt_intern.hh"
+
+#include "BLI_float4x4.hh"
+#include "BLI_index_mask_ops.hh"
+#include "BLI_kdtree.h"
+#include "BLI_rand.hh"
+#include "BLI_vector.hh"
+
+#include "PIL_time.h"
+
+#include "DEG_depsgraph.h"
+
+#include "BKE_attribute_math.hh"
+#include "BKE_brush.h"
+#include "BKE_bvhutils.h"
+#include "BKE_context.h"
+#include "BKE_curves.hh"
+#include "BKE_mesh.h"
+#include "BKE_mesh_runtime.h"
+#include "BKE_paint.h"
+
+#include "DNA_brush_enums.h"
+#include "DNA_brush_types.h"
+#include "DNA_curves_types.h"
+#include "DNA_mesh_types.h"
+#include "DNA_meshdata_types.h"
+#include "DNA_object_types.h"
+#include "DNA_screen_types.h"
+#include "DNA_space_types.h"
+
+#include "ED_screen.h"
+#include "ED_view3d.h"
+
+#include "UI_interface.h"
+
+#include "WM_api.h"
 
 namespace blender::ed::sculpt_paint {
 
+class SlideOperation : public CurvesSculptStrokeOperation {
+ private:
+  /** Only used when a 3D brush is used. */
+  CurvesBrush3D brush_3d_;
+
+  friend struct SlideOperationExecutor;
+
+ public:
+  void on_stroke_extended(const bContext &C, const StrokeExtension &stroke_extension) override;
+};
+
+/**
+ * Utility class that actually executes the update when the stroke is updated. That's useful
+ * because it avoids passing a very large number of parameters between functions.
+ */
+struct SlideOperationExecutor {
+  SlideOperation *self_ = nullptr;
+  CurvesSculptCommonContext ctx_;
+
+  SlideOperationExecutor(const bContext &C) : ctx_(C)
+  {
+  }
+
+  void execute(SlideOperation &self, const bContext &C, const StrokeExtension &stroke_extension)
+  {
+    UNUSED_VARS(C, stroke_extension);
+    self_ = &self;
+  }
+};
+
+void SlideOperation::on_stroke_extended(const bContext &C, const StrokeExtension &stroke_extension)
+{
+  SlideOperationExecutor executor{C};
+  executor.execute(*this, C, stroke_extension);
+}
+
 std::unique_ptr<CurvesSculptStrokeOperation> new_slide_operation()
 {
-  return {};
+  return std::make_unique<SlideOperation>();
 }
 
 }  // namespace blender::ed::sculpt_paint
