77bee038a6b48f0bb44ad05d17d8ff69104422e1
Jacques Lucke <jacques@blender.org>
2022-06-03 16:13:19 +0200

    add density boilerplate

--- source/blender/editors/sculpt_paint/curves_sculpt_density.cc
+++ source/blender/editors/sculpt_paint/curves_sculpt_density.cc
@@ -4,9 +4,46 @@
 
 namespace blender::ed::sculpt_paint {
 
+class DensityOperation : public CurvesSculptStrokeOperation {
+ private:
+  /** Only used when a 3D brush is used. */
+  CurvesBrush3D brush_3d_;
+
+  friend struct DensityOperationExecutor;
+
+ public:
+  void on_stroke_extended(const bContext &C, const StrokeExtension &stroke_extension) override;
+};
+
+/**
+ * Utility class that actually executes the update when the stroke is updated. That's useful
+ * because it avoids passing a very large number of parameters between functions.
+ */
+struct DensityOperationExecutor {
+  DensityOperation *self_ = nullptr;
+  CurvesSculptCommonContext ctx_;
+
+  DensityOperationExecutor(const bContext &C) : ctx_(C)
+  {
+  }
+
+  void execute(DensityOperation &self, const bContext &C, const StrokeExtension &stroke_extension)
+  {
+    UNUSED_VARS(C, stroke_extension);
+    self_ = &self;
+  }
+};
+
+void DensityOperation::on_stroke_extended(const bContext &C,
+                                          const StrokeExtension &stroke_extension)
+{
+  DensityOperationExecutor executor{C};
+  executor.execute(*this, C, stroke_extension);
+}
+
 std::unique_ptr<CurvesSculptStrokeOperation> new_density_operation()
 {
-  return {};
+  return std::make_unique<DensityOperation>();
 }
 
 }  // namespace blender::ed::sculpt_paint
