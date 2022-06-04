b9c2e2552bab43d64e00340860423f07b4da701a
Simon Thommes <simon.thommes@gmx.de>
2022-06-04 20:19:57 +0200

    puff brush boilerplate

--- source/blender/editors/sculpt_paint/curves_sculpt_puff.cc
+++ source/blender/editors/sculpt_paint/curves_sculpt_puff.cc
@@ -1,5 +1,19 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include "BKE_brush.h"
+#include "BKE_context.h"
+
+#include "ED_screen.h"
+#include "ED_view3d.h"
+
+#include "DEG_depsgraph.h"
+
+#include "DNA_brush_types.h"
+
+#include "WM_api.h"
+
+#include "BLI_enumerable_thread_specific.hh"
+
 #include "curves_sculpt_intern.hh"
 
 namespace blender::ed::sculpt_paint {
@@ -23,6 +37,27 @@ struct PuffOperationExecutor {
   PuffOperation *self_ = nullptr;
   CurvesSculptCommonContext ctx_;
 
+  Object *object_ = nullptr;
+  Curves *curves_id_ = nullptr;
+  CurvesGeometry *curves_ = nullptr;
+
+  VArray<float> point_factors_;
+  Vector<int64_t> selected_curve_indices_;
+  IndexMask curve_selection_;
+
+  const CurvesSculpt *curves_sculpt_ = nullptr;
+  const Brush *brush_ = nullptr;
+  float brush_radius_base_re_;
+  float brush_radius_factor_;
+  float brush_strength_;
+  float2 brush_pos_re_;
+
+  eBrushFalloffShape falloff_shape_;
+  eBrushCurvesSculptSmoothMode smooth_mode_;
+
+  float4x4 curves_to_world_mat_;
+  float4x4 world_to_curves_mat_;
+
   PuffOperationExecutor(const bContext &C) : ctx_(C)
   {
   }
@@ -31,6 +66,48 @@ struct PuffOperationExecutor {
   {
     UNUSED_VARS(C, stroke_extension);
     self_ = &self;
+
+    object_ = CTX_data_active_object(&C);
+
+    curves_id_ = static_cast<Curves *>(object_->data);
+    curves_ = &CurvesGeometry::wrap(curves_id_->geometry);
+    if (curves_->curves_num() == 0) {
+      return;
+    }
+
+    curves_sculpt_ = ctx_.scene->toolsettings->curves_sculpt;
+    brush_ = BKE_paint_brush_for_read(&curves_sculpt_->paint);
+    brush_radius_base_re_ = BKE_brush_size_get(ctx_.scene, brush_);
+    brush_radius_factor_ = brush_radius_factor(*brush_, stroke_extension);
+    brush_strength_ = brush_strength_get(*ctx_.scene, *brush_, stroke_extension);
+    brush_pos_re_ = stroke_extension.mouse_position;
+
+    point_factors_ = get_point_selection(*curves_id_);
+    curve_selection_ = retrieve_selected_curves(*curves_id_, selected_curve_indices_);
+
+    curves_to_world_mat_ = object_->obmat;
+    world_to_curves_mat_ = curves_to_world_mat_.inverted();
+
+    falloff_shape_ = static_cast<eBrushFalloffShape>(brush_->falloff_shape);
+    smooth_mode_ = static_cast<eBrushCurvesSculptSmoothMode>(
+        brush_->curves_sculpt_settings->smooth_mode);
+
+    if (stroke_extension.is_first) {
+      if (falloff_shape_ == PAINT_FALLOFF_SHAPE_SPHERE) {
+        self.brush_3d_ = *sample_curves_3d_brush(*ctx_.depsgraph,
+                                                 *ctx_.region,
+                                                 *ctx_.v3d,
+                                                 *ctx_.rv3d,
+                                                 *object_,
+                                                 brush_pos_re_,
+                                                 brush_radius_base_re_);
+      }
+    }
+
+    curves_->tag_positions_changed();
+    DEG_id_tag_update(&curves_id_->id, ID_RECALC_GEOMETRY);
+    WM_main_add_notifier(NC_GEOM | ND_DATA, &curves_id_->id);
+    ED_region_tag_redraw(ctx_.region);
   }
 };
 
