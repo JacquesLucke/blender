35980d233f663552ea793dc1aedb01e1d3c88cdb
Simon Thommes <simon.thommes@gmx.de>
2022-06-04 13:27:28 +0200

    Revert "improve"
    This reverts commit b3d2f68cfc0b083dff98a926044b9b4056ed9af4.

--- source/blender/geometry/GEO_reverse_uv_lookup.hh
+++ source/blender/geometry/GEO_reverse_uv_lookup.hh
@@ -4,10 +4,8 @@
 
 #include <optional>
 
-#include "BLI_array.hh"
 #include "BLI_math_vector.hh"
 #include "BLI_span.hh"
-#include "BLI_vector.hh"
 
 #include "DNA_meshdata_types.h"
 
@@ -17,21 +15,6 @@ class ReverseUVLookup {
  private:
   const Span<float2> uv_map_;
   const Span<MLoopTri> looptris_;
-
-  /** This is just an initial implementation to avoid quadratic run-time behavior. */
-  enum class CellStatus {
-    EmptyOrPartial,
-    Full,
-    FullInvalid,
-  };
-  struct Cell {
-    CellStatus status = CellStatus::EmptyOrPartial;
-    Vector<int, 2> looptris;
-  };
-  int grid_resolution_ = 0;
-  Array<Cell> grid_;
-  float2 grid_offset_;
-  float2 grid_scale_;
 
  public:
   ReverseUVLookup(const Span<float2> uv_map, const Span<MLoopTri> looptris);
@@ -49,11 +32,6 @@ class ReverseUVLookup {
   };
 
   Result lookup(const float2 &query_uv) const;
-
- private:
-  int2 uv_to_cell_coord(const float2 &uv) const;
-  float2 cell_coord_to_uv(const int2 &cell_coord) const;
-  bool tri_covers_cell(const int2 &cell_coord, const std::array<float2, 3> &tri) const;
 };
 
 }  // namespace blender::geometry
