bc78c0ec25064b40abaa4cb8801b5e62cff82e58
Jacques Lucke <jacques@blender.org>
2022-06-22 15:21:55 +0200

    progress

--- source/blender/nodes/geometry/nodes/node_geo_sample_mesh_deformation.cc
+++ source/blender/nodes/geometry/nodes/node_geo_sample_mesh_deformation.cc
@@ -158,17 +158,11 @@ class SampleMeshDeformationFunction : public fn::MultiFunction {
         copy_v3_v3(new_transform.values[1], new_tangent_y);
         copy_v3_v3(new_transform.values[2], new_normal);
 
-        float old_quat[4];
-        mat3_normalized_to_quat(old_quat, old_transform.values);
-        float new_quat[4];
-        mat3_normalized_to_quat(new_quat, new_transform.values);
-
-        float quat[4];
-        rotation_between_quats_to_quat(quat, old_quat, new_quat);
+        const float3x3 old_transform_inverse = old_transform.transposed();
+        const float3x3 transform = new_transform * old_transform_inverse;
 
         float3 euler;
-        quat_to_eul(euler, quat);
-
+        mat3_to_eul(euler, transform.values);
         r_rotations[i] = euler;
       }
     }
@@ -186,8 +180,6 @@ static void node_geo_exec(GeoNodeExecParams params)
     params.set_default_remaining_outputs();
     return;
   }
-
-  std::cout << "test\n";
 
   geometry.ensure_owns_direct_data();
 
