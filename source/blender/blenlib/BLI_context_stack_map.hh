cec0db55df6af70b87dd03c27fd0d6bf97bf5b57
Jacques Lucke <jacques@blender.org>
2022-06-26 10:29:24 +0200

    cleanup

--- source/blender/blenlib/BLI_context_stack_map.hh
+++ source/blender/blenlib/BLI_context_stack_map.hh
@@ -12,43 +12,26 @@
 
 namespace blender {
 
-namespace context_stack_map_detail {
-template<typename T> struct Value {
-  T value;
-  const char *static_type;
-  std::optional<ContextStackHash> parent_hash;
-};
-}  // namespace context_stack_map_detail
-
 template<typename T> class ContextStackMap {
  private:
-  using Value = context_stack_map_detail::Value<T>;
-
   LinearAllocator<> allocator_;
-  Map<ContextStackHash, destruct_ptr<Value>> map_;
+  Map<ContextStackHash, destruct_ptr<T>> map_;
 
  public:
   T &lookup_or_add(const ContextStack &context_stack)
   {
     const ContextStackHash &hash = context_stack.hash();
-    destruct_ptr<Value> &value = map_.lookup_or_add_cb(hash, [&]() {
-      destruct_ptr<Value> value = allocator_.construct<Value>();
-      value->static_type = context_stack.static_type();
-      const ContextStack *parent = context_stack.parent();
-      if (parent != nullptr) {
-        value->parent_hash = parent->hash();
-      }
-      return value;
-    });
-    return value->value;
+    destruct_ptr<T> &value = map_.lookup_or_add_cb(hash,
+                                                   [&]() { return allocator_.construct<T>(); });
+    return *value;
   }
 
   const T &lookup_or_default(const ContextStack &context_stack, const T &default_value) const
   {
     const ContextStackHash &hash = context_stack.hash();
-    const destruct_ptr<Value> *value = map_.lookup_ptr(hash);
+    const destruct_ptr<T> *value = map_.lookup_ptr(hash);
     if (value != nullptr) {
-      return value->get()->value;
+      return **value;
     }
     return default_value;
   }
