1f6fbe9a96841e426f7cbf9939fac4cb405fd562
Jacques Lucke <jacques@blender.org>
2022-07-28 13:32:54 +0200

    progress

--- source/blender/blenlib/intern/proxy_value_cache.cc
+++ source/blender/blenlib/intern/proxy_value_cache.cc
@@ -18,7 +18,7 @@ struct GlobalProxyValueCache {
 };
 
 struct LocalProxyValueCache {
-  int version;
+  int version = 0;
   Set<ProxyValue> cached_values;
 };
 
@@ -44,13 +44,29 @@ void update_memory_limit(const uint64_t new_limit)
 std::shared_ptr<CachedValue> lookup(const ProxyValue &proxy)
 {
   GlobalProxyValueCache &global_cache = get_global_cache();
+  LocalProxyValueCache &local_cache = get_local_cache();
+
+  if (local_cache.version < global_cache.version_counter.load(std::memory_order_relaxed)) {
+    local_cache.cached_values.clear();
+    std::lock_guard lock{global_cache.mutex};
+    for (const ProxyValue &proxy : global_cache.cache_map.keys()) {
+      local_cache.cached_values.add(proxy);
+    }
+    local_cache.version = global_cache.version_counter;
+  }
+  if (!local_cache.cached_values.contains(proxy)) {
+    return {};
+  }
+  std::lock_guard lock{global_cache.mutex};
   return global_cache.cache_map.lookup_default(proxy, {});
 }
 
 void force_cache(const ProxyValue &proxy, std::shared_ptr<CachedValue> value)
 {
   GlobalProxyValueCache &global_cache = get_global_cache();
+  std::lock_guard lock{global_cache.mutex};
   global_cache.cache_map.add_overwrite(proxy, std::move(value));
+  global_cache.version_counter.fetch_add(1);
 }
 
 }  // namespace blender::proxy_value_cache
